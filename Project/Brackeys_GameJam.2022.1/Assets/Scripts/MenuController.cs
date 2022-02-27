498e5bcce24f59626c8436df84824e3966a47cbb
Skynex457 <andrey_zhabunin@mail.ru>
2022-02-27 11:48:29 +0300

    Menu

--- Project/Brackeys_GameJam.2022.1/Assets/Scripts/MenuController.cs
+++ Project/Brackeys_GameJam.2022.1/Assets/Scripts/MenuController.cs
@@ -5,14 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    [SerializeField] public AudioSource audioSource;
+    [SerializeField] public AudioClip klickSound;
+    [SerializeField] public AudioClip hoverSound;
     public void Play()
     {
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
     }
 
     public void Exit()
-    {
-        Debug.Log("Exit"); 
+    { 
+        
         Application.Quit();
     }
+
+    public void Klick()
+    {
+        audioSource.PlayOneShot(klickSound);
+    }
+
+    public void Hover()
+    {
+        audioSource.PlayOneShot(hoverSound);
+    }
+
+  
+    
 }
+
